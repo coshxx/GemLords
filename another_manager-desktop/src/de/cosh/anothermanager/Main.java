3d59fb32b62c6fbb7d34045a06ff2846e05072f2
cosh <coshxx@googlemail.com>
2013-12-06 11:46:22 +0100

    initial

---
+++ another_manager-desktop/src/de/cosh/anothermanager/Main.java
@@ -0,0 +1,16 @@
+package de.cosh.anothermanager;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "another_manager";
+		cfg.useGL20 = false;
+		cfg.width = 1024;
+		cfg.height = 640;
+		
+		new LwjglApplication(new AnotherManager(), cfg);
+	}
+}
---
+++ another_manager/src/de/cosh/anothermanager/AnotherManager.java
@@ -0,0 +1,44 @@
+package de.cosh.anothermanager;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class AnotherManager implements ApplicationListener {
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture texture;
+	private Sprite sprite;
+	
+	@Override
+	public void create() {		
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void render() {		
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
