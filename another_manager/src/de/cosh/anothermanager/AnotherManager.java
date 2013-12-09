976c85dd5f7aeed983676dd4f311f1bf8eaa9633
cosh <coshxx@googlemail.com>
2013-12-09 08:10:11 +0100

    test2

--- another_manager/src/de/cosh/anothermanager/AnotherManager.java
+++ another_manager/src/de/cosh/anothermanager/AnotherManager.java
@@ -28,6 +28,10 @@ public class AnotherManager implements ApplicationListener {
 	public void render() {		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		
 	}
 
 	@Override
