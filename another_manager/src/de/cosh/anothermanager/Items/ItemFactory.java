2159d4826838d5196392200400068297575491f7
cosh <coshxx@googlemail.com>
2014-01-18 16:15:54 +0100

    wip

---
+++ another_manager/src/de/cosh/anothermanager/Items/ItemFactory.java
@@ -0,0 +1,19 @@
+package de.cosh.anothermanager.Items;
+
+import de.cosh.anothermanager.AnotherManager;
+
+/**
+ * Created by cosh on 17.01.14.
+ */
+public class ItemFactory {
+    private AnotherManager myGame;
+
+    public ItemFactory(AnotherManager myGame) {
+        this.myGame = myGame;
+    }
+
+    public Item generateRobe() {
+        Item item = new Item(myGame, Item.ItemType.ITEM_ROBE);
+        return item;
+    }
+}
