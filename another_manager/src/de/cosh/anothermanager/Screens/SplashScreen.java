56e9cf0c14f066f5b76929dc09f757497b382d44
cosh <coshxx@googlemail.com>
2014-01-21 15:36:45 +0100

    wip

--- another_manager/src/de/cosh/anothermanager/Screens/SplashScreen.java
+++ another_manager/src/de/cosh/anothermanager/Screens/SplashScreen.java
@@ -86,6 +86,7 @@ public class SplashScreen implements Screen {
 		myGame.assets.load("data/textures/splash.jpg", Texture.class);
 		myGame.assets.load("data/textures/cell_back.png", Texture.class);
 		myGame.assets.load("data/textures/enemy.png", Texture.class);
+		myGame.assets.load("data/textures/enemyscarecrow.png", Texture.class);
 		myGame.assets.load("data/textures/enemysnake.png", Texture.class);
 		myGame.assets.load("data/textures/enemyrat.png", Texture.class);
 		myGame.assets.load("data/textures/star.png", Texture.class);
